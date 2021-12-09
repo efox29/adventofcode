7637bbaaba696ea98398f84b1b46360a82db0e05
Enrico Perera <enrico.perera@rometlimited.com>
2021-12-09 01:36:02 -0500

    day 2: part 2 complete

--- 2021/day2/day2.py
+++ 2021/day2/day2.py
@@ -3,6 +3,7 @@ file1 = open('input.txt','r')
 hor_direction = 0
 ver_direction = 0
 
+
 for line in file1:
     token = line.split(" ")
     val = int(token[1])
@@ -13,5 +14,32 @@ for line in file1:
     elif token[0] == 'up':
         ver_direction -= val
     
-print(f'  h = {hor_direction}\nx v = {ver_direction}\n{hor_direction*ver_direction}')
+print(f'Part1\n  h = {hor_direction}\nx v = {ver_direction}\n{hor_direction*ver_direction}')
+file1.close()
+
+##########
+
+file1 = open('input.txt','r')
+
+hor_direction = 0
+ver_direction = 0
+aim = 0
+
+
+for line in file1:
+    token = line.split(" ")
+    val = int(token[1])
+    if token[0] == "forward":
+        hor_direction +=val
+        ver_direction += (val * aim)
+       
+    elif token[0] == 'down':     
+        aim += val
+        
+    elif token[0] == 'up':       
+        aim -= val
+       
+   
+    
+print(f'Part2\n  h = {hor_direction}\nx v = {ver_direction}\n{hor_direction*ver_direction}')
 file1.close()
